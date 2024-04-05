b7476137937296c4e1d13c29baacb669c0e838bc
JHONATAS BASTOS CAMARGO <jhonatas.7942@aluno.pr.senac.br>
2024-04-05 11:10:08 -0300

    adicionado ou alterado

--- src/assets/pages/sub-pages/Filmes.tsx
+++ src/assets/pages/sub-pages/Filmes.tsx
@@ -2,7 +2,7 @@ function Filmes() {
     return(
         <>
             <div className="filmes">
-                <h1>Filmes</h1>
+                <div className="filme1"></div>
             </div>
         </>
     );
