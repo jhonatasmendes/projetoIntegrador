4c8fa4d77726651bbfd344554737bd9dbd54014b
JHONATAS BASTOS CAMARGO <jhonatas.7942@aluno.pr.senac.br>
2024-04-15 11:12:59 -0300

    modificado

--- src/assets/pages/FrontPage.tsx
+++ src/assets/pages/FrontPage.tsx
@@ -19,12 +19,12 @@ function FrontPage() {
         <>
             <div className="content">
                 <div className="side-bar">
-                    <NavBarItem onClick={main}>Main</NavBarItem>
                     <a href="https://ccnew.online/"><NavBarItem>Painel Trend</NavBarItem></a>
                     <a href="https://ccnew.online/"><NavBarItem>Painel GP</NavBarItem></a>
                     <a href="https://infinityplay.qpanel.top/#/sign-in"><NavBarItem>Painel TF</NavBarItem></a>
                     <a href="https://tf1james.apptftv.workers.dev/0:/App/"><NavBarItem>APKS</NavBarItem></a>
 
+                    <NavBarItem onClick={main}>Main</NavBarItem>
                     <NavBarItem onClick={films}>Filmes</NavBarItem>
                 </div>
                 
