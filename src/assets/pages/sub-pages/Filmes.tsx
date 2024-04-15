373ea449894120288b85d558f660eeffbc182ae2
JHONATAS BASTOS CAMARGO <jhonatas.7942@aluno.pr.senac.br>
2024-04-15 10:33:21 -0300

    modificado

--- src/assets/pages/sub-pages/Filmes.tsx
+++ src/assets/pages/sub-pages/Filmes.tsx
@@ -1,10 +1,12 @@
 import FilmItem from "../../components/FIlmItem";
+import "./Filmes.css"
 
 function Filmes() {
     return(
         <>
             <div className="filmes">
-                <FilmItem/>
+                <FilmItem url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ61Tx96ROgXiLJo_nTz_wXlw1MOPianZ5cJhYhFfX69A&s">Filme 1</FilmItem>
+                <FilmItem url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzg0V3CIqUSpftS61mcrmHkPtPOT3k2GSanQuI461Ijg&s">Filme 2</FilmItem>
             </div>
         </>
     );
