1dd8dfb0c560e212baa516c8df40fea7ed649f9b
JHONATAS BASTOS CAMARGO <jhonatas.7942@aluno.pr.senac.br>
2024-05-06 08:51:07 -0300

    modificado

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,8 @@ import FrontPage from './assets/pages/FrontPage';
 import Login from './assets/pages/Login'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PainelADM from './assets/pages/PainelADM';
-import axios from 'axios';
-import { useState } from 'react';
-
-interface Reven {
-  id: number
-  master: string
-};
 
 function App() {
-  const [reven, setReven] = useState<Reven[]>([]);
-
-  const handleGetReven = async () => {
-    const res = await axios.get("http://localhost:8080/reven")
-
-    setReven(res.data)
-  };
-
-  window.onload = handleGetReven;
-
-  console.log(reven)
-
   return (
     <>
       <BrowserRouter>
--- src/assets/pages/FrontPage.tsx
+++ src/assets/pages/FrontPage.tsx
@@ -1,19 +1,26 @@
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes} from "react-router-dom";
 import NavBarItem from "../components/NavBarItem";
 import "./FrontPage.css";
 import Main from "./sub-pages/Main";
 import Filmes from "./sub-pages/Filmes";
+import MyButton from "../components/MyButton";
+import axios from 'axios';
+import { useState } from 'react';
+
+interface Reven {
+    id: number
+    master: string
+};
 
 function FrontPage() {
-    const navigate = useNavigate();
+    const [reven, setReven] = useState<Reven[]>([]);
 
-    const main = () => {
-        navigate("")
-    }
-
-    const films = () => {
-        navigate("films")
-    }
+    const handleGetReven = async () => {
+        const res = await axios.get("http://localhost:8080/reven")
+        setReven(res.data)
+  
+        console.log(reven)
+    };
 
     return (
         <>
@@ -24,7 +31,8 @@ function FrontPage() {
                 <a href="https://paineltftv.projetojmmidias.workers.dev/0:/S%C3%A9rie/"><NavBarItem>Séries</NavBarItem></a>
                 <a href="https://paineltftv.projetojmmidias.workers.dev/0:/Jogos/"><NavBarItem>Jogos</NavBarItem></a>
                 <a href="https://drive.google.com/drive/u/8/folders/10hQxcG_ongsDND56nKfx4dEJnheYKFI9"><NavBarItem>APK</NavBarItem></a>
-                </div>
+                <MyButton onClick={handleGetReven}>Dados</MyButton>
+            </div>
                 
                 <Routes>
                     <Route path="/" element={<Main/>}></Route>
