018ec835de0fbb8f3484b6dfae1f087b49105043
jhonatasmendes <liberalinojhonatas@gmail.com>
2024-05-08 09:35:16 -0300

    Link Modificado

--- src/assets/pages/FrontPage.tsx
+++ src/assets/pages/FrontPage.tsx
@@ -30,6 +30,7 @@ function FrontPage() {
                     <a href="https://paineltftv.projetojmmidias.workers.dev/0:/Filmes/"><NavBarItem>Filmes</NavBarItem></a>
                     <a href="https://paineltftv.projetojmmidias.workers.dev/0:/S%C3%A9rie/"><NavBarItem>Séries</NavBarItem></a>
                     <a href="https://paineltftv.projetojmmidias.workers.dev/0:/Jogos/"><NavBarItem>Jogos</NavBarItem></a>
+                    <a href="https://paineltftv.projetojmmidias.workers.dev/0:/APK/"><NavBarItem>Jogos</NavBarItem></a>
                     {/* <a href="https://drive.google.com/drive/u/8/folders/10hQxcG_ongsDND56nKfx4dEJnheYKFI9"><NavBarItem>APK</NavBarItem></a> */}
                     <MyButton onClick={handleGetReven}>Dados</MyButton>
                 </div>
